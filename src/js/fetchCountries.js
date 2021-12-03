894c2218a61932ff73aa782c3a3cd9fd8ba23190
Bohdana-Halenko <danaermolenko20@gmail.com>
2021-12-03 09:30:12 +0200

    add

--- src/js/fetchCountries.js
+++
@@ -1,8 +0,0 @@
-function fetchCountries(name) {
-    if (name.length)
-        return fetch(
-            `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`,
-        ).then(response => response.json());
-}
-
-export {fetchCountries};
